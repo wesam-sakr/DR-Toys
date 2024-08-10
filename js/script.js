743b15609a2b06f75643e72c550c0d94c1c1145a
wesam-sakr <wesamsakr801@gmail.com>
2024-08-10 19:35:25 +0300

    start home

---
+++ js/script.js
@@ -0,0 +1,172 @@
+$(document).ready(function () {
+  var bodyDir = $('body').css('direction')
+  var dirAr
+  if (bodyDir == "rtl") {
+    dirAr = true
+  }
+  else {
+    dirAr = false
+  }
+
+
+  // loading
+
+  $("body").css('overflow-y', 'auto');
+
+  $('#loading').fadeOut(500);
+
+  $('select').niceSelect();
+
+  $(".fav").click(function () {
+    $(this).children(".fa-heart").toggleClass('fa-regular').toggleClass('fa-solid');
+  });
+
+  $('.toggleFav').click(function () {
+    $(this).toggleClass('added')
+  })
+
+
+  // ----- scroll top button ------
+
+  var btn_top = $('#scroll-top');
+  var btn_bottom = $('.scroll-bottom');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn_top.addClass('show');
+    } else {
+      btn_top.removeClass('show');
+    }
+  })
+
+  btn_top.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+
+  btn_bottom.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 730 }, '300');
+  });
+
+
+  const inputElements = [...document.querySelectorAll("input.code")];
+  inputElements.forEach((ele, index) => {
+    ele.addEventListener("keydown", (e) => {
+      if (e.keyCode === 8 && e.target.value === "")
+        inputElements[Math.max(0, index - 1)].focus();
+    });
+    ele.addEventListener("input", (e) => {
+      inputElements[index].focus();
+      const [first, ...rest] = e.target.value;
+      e.target.value = first ?? ""; // first will be undefined when backspace was entered, so set the input to ""
+      const lastInputBox = index === inputElements.length - 1;
+      const didInsertContent = first !== undefined;
+      if (didInsertContent && !lastInputBox) {
+        inputElements[index + 1].focus();
+        inputElements[index + 1].value = rest.join("");
+        inputElements[index + 1].dispatchEvent(new Event("input"));
+      }
+    });
+  });
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const code = inputElements.map(({ value }) => value).join("");
+    console.log(code);
+  }
+
+  $(".banner-slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    rtl: dirAr,
+    // autoplay: true,
+    dots :true,
+    autoplayHoverPause: true,
+  });
+
+  $(".suggest .owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 24,
+    items: 5,
+    rtl: dirAr,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false
+      },
+      768: {
+        items: 3,
+        nav: false
+      },
+      998: {
+        items: 4,
+        nav: false,
+      }
+    }
+  });
+
+  $("#your-rate").rateYo({
+    starWidth: "15px",
+    ratedFill: "#FFC107",
+    rating: 0,
+    fullStar: true,
+    rtl: dirAr
+  });
+
+
+  $(".add-address").click(function () {
+    if ($(".form-address").css("display") == "block") {
+      $(".form-address").hide();
+    } else {
+      $(".form-address").show();
+    }
+  });
+  $(".input-cridet").hide();
+
+  $('.cart input[type="radio"]').change(function () {
+    if ($(".lable-cridet").is(":checked")) {
+      $(".input-cridet").show();
+    } else {
+      $(".input-cridet").hide();
+    }
+  });
+
+  $("#filter").click(function () {
+    $(".filter").toggleClass("filter-toggle");
+  });
+  $(".filter-header .btn-close").click(function () {
+    $(".filter").toggleClass("filter-toggle");
+  });
+
+  $("#profile_nav").click(function () {
+    $(".profile-nav").toggleClass("Pnav-toggle");
+  });
+  $(".profile-header .btn-close").click(function () {
+    $(".profile-nav").toggleClass("Pnav-toggle");
+  });
+
+  /* -------------- upload profile pic ---------------- */
+  if ($(".profile-pic").length > 0) {
+    const imgDiv = document.querySelector(".profile-pic");
+    const img = document.querySelector("#photo");
+    const file = document.querySelector("#file");
+    const uploadBtn = document.querySelector("#uploadBtn");
+
+    //when we choose a pic to upload
+
+    file.addEventListener("change", function () {
+      const choosedFile = this.files[0];
+      if (choosedFile) {
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+          img.setAttribute("src", reader.result);
+        });
+        reader.readAsDataURL(choosedFile);
+      }
+    });
+  }
+
+});
